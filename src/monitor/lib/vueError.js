0af9ef05709b49a5475b02f7502f9eb128996784
Liu_yunnan <1792147770@qq.com>
2022-08-09 20:06:33 +0800

    vueerror

--- src/monitor/lib/vueError.js
+++ src/monitor/lib/vueError.js
@@ -1,17 +1,16 @@
 import tracker from '../utils/tracker'
-export function injectVueError (app) {
-  app.config.errorHandler = (error, vm, info) => {
-    const componentName = formatComponentName(vm)
-    const exception = {
-      kind: 'stability', // 监控指标的大类：稳定性指标
-      type: 'error', // 小类型 这是一个错误
-      errorType: 'vueError', // 资源加载错误
-      value: error.message,// 错误信息
-      componentName,// 报错的Vue组件名
-      hook: info,// 报错的Vue阶段
-    }
-    tracker.send(exception)
+export function injectVueError (error, vm, info) {
+  const componentName = formatComponentName(vm)
+  const exception = {
+    kind: 'stability', // 监控指标的大类：稳定性指标
+    type: 'error', // 小类型 这是一个错误
+    errorType: 'vueError', // 资源加载错误
+    value: error.message,// 错误信息
+    componentName,// 报错的Vue组件名
+    hook: info,// 报错的Vue阶段
   }
+  tracker.send(exception)
+
   // 获取报错组件名
   const classifyRE = /(?:^|[-_])(\w)/g
   const classify = (str) => str.replace(classifyRE, (c) => c.toUpperCase()).replace(/[-_]/g, '')
@@ -38,5 +37,4 @@ export function injectVueError (app) {
       (name ? `<${classify(name)}>` : ANONYMOUS_COMPONENT_NAME) + (file && includeFile !== false ? ` at ${file}` : '')
     )
   }
-
 }
